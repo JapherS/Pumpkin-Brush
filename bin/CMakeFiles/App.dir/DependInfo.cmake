f702b95b9bda9c9cbc097aa1ee5c8509a0916af2
Japher Su <japhersu@Japhers-MacBook-Pro.local>
2021-05-21 15:24:15 -0400

    added project

---
+++ bin/CMakeFiles/App.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/App.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/App.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/Brush.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/Brush.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/BrushFactory.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/BrushFactory.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/Clear.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/Clear.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/Command.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/Command.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/Draw.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/Draw.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/Erase.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/Erase.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/Gui.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/Gui.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/Pen.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/Pen.cpp.o"
+  "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/src/main.cpp" "/Users/japhersu/Desktop/CS5500 GitHub/CS5500-Foundations-of-Software-Engineering/Pumpkin Brush/FinalProject/bin/CMakeFiles/App.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "/Library/Frameworks/SFML.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
